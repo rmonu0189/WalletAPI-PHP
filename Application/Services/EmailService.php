5a593c46ba7f2732e2584f417925dd47feb276a6
monudianapps <monu.rathore@dianapps.com>
2023-03-11 16:35:50 +0530

    Add email service for OTP

--- Application/Services/EmailService.php
+++ Application/Services/EmailService.php
@@ -3,7 +3,13 @@ namespace Application\Services;
 
 class EmailService {
     public static function sendOTP($email, $otp) {
-        $headers = "From: no-reply@digimoplus.online";
+        // Set content-type header for sending HTML email 
+        $headers = "MIME-Version: 1.0" . "\r\n"; 
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+        
+        // Additional headers 
+        $headers .= 'From: no-reply@digimoplus.online' . "\r\n"; 
+
         $body = otpTemplate($otp);
         mail($email, "WalletManagement: Recover your password.", $body, $headers);
     }
